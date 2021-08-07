6b008906af7fec584c0c3b26e424a526f7bc0312
Farhan Syah <45132618+farhan-syah@users.noreply.github.com>
2021-08-07 15:46:13 +0800

    - Update Task Container - Add description property to Task model

--- lib/screen/home.screen.dart
+++ lib/screen/home.screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_note/model/task.model.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -8,10 +9,11 @@ class HomeScreen extends StatelessWidget {
         title: Text('Awesome List App'),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: List.generate(
           taskList.length,
           (index) {
-            return CustomText(text: taskList[index]);
+            return TaskContainer(task: taskList[index]);
           },
         ),
       ),
@@ -19,25 +21,38 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
-class CustomText extends StatelessWidget {
-  final String text;
+class TaskContainer extends StatelessWidget {
+  final Task task;
 
-  CustomText({required this.text});
+  TaskContainer({required this.task});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Text(
-        text,
-        style: TextStyle(fontSize: 30),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey.shade200,
+          border: Border.all(color: Colors.grey.shade300),
+        ),
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              task.title,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Text(
+              task.description,
+              style: TextStyle(fontSize: 20),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
-List<String> taskList = [
-  'Task 1',
-  'Task 2',
-  'Task 3',
-];
