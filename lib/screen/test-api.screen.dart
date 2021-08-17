8db606500da4682a307daed67c20b5953d9ed2ca
Farhan Syah <45132618+farhan-syah@users.noreply.github.com>
2021-08-17 20:51:23 +0800

    - Convert data from getTodo api to List<Todo> object - FutureBuilder to use that data to properly diplay the data in the UI

--- lib/screen/test-api.screen.dart
+++ lib/screen/test-api.screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_note/model/todo.model.dart';
 import 'package:flutter_note/service/api.dart';
 
 class TestApiScreen extends StatelessWidget {
@@ -10,10 +11,41 @@ class TestApiScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Screen to Test API'),
       ),
-      body: FutureBuilder<dynamic>(
+      body: FutureBuilder<List<Todo>>(
           future: getTodo(),
           builder: (context, snapshot) {
-            return Container();
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              List<Todo> todoList = snapshot.data!;
+              return ListView.builder(
+                itemCount: todoList.length,
+                itemBuilder: (context, index) {
+                  final Todo todo = todoList[index];
+                  return Container(
+                    padding: EdgeInsets.all(5),
+                    child: Row(
+                      children: [
+                        Flexible(
+                            fit: FlexFit.loose,
+                            child: Text('${todo.id}. ${todo.title}')),
+                        todo.completed
+                            ? Icon(
+                                Icons.check,
+                                color: Colors.green,
+                              )
+                            : Icon(
+                                Icons.close,
+                                color: Colors.red,
+                              ),
+                      ],
+                    ),
+                  );
+                },
+              );
+            }
           }),
     );
   }
--- lib/service/api.dart
+++ lib/service/api.dart
@@ -1,12 +1,15 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_note/model/todo.model.dart';
 
-getTodo() async {
+Future<List<Todo>> getTodo() async {
   try {
-    var response =
+    Response response =
         await Dio().get('https://jsonplaceholder.typicode.com/todos');
-    print(response); // dalam bentuk dynamic? Kena convert kepada object Task
-    //  return list ; // List<Todo>
+    List<dynamic> data = response.data;
+    List<Todo> todoList = data.map((e) => Todo.fromMap(e)).toList();
+    return todoList;
   } catch (e) {
     print(e);
+    return [];
   }
 }
