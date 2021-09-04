7f29e1765e5a1a8cb85a9d2c5f6f7018dd161575
Farhan Syah <45132618+farhan-syah@users.noreply.github.com>
2021-09-04 14:42:16 +0800

    Update Icon Add Firebase Cloud Messaging

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:camera/camera.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_note/providers/user.provider.dart';
 import 'package:flutter_note/screen/landing-page.screen.dart';
@@ -8,10 +11,73 @@ import 'package:provider/provider.dart';
 
 late List<CameraDescription> cameras;
 
+// Firebase Messaging Instance
+FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+// Firebase Background Handler
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+
+  print("Handling a background message: ${message.messageId}");
+}
+
+// Firebase Foregound
+// const AndroidNotificationChannel channel = AndroidNotificationChannel(
+//   'high_importance_channel', // id
+//   'High Importance Notifications', // title
+//   'This channel is used for important notifications.', // description
+//   importance: Importance.max,
+// );
+
 void main() async {
   await GetStorage.init();
   await Firebase.initializeApp();
   cameras = await availableCameras();
+
+  if (Platform.isIOS) {
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+  }
+  // final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+  //     FlutterLocalNotificationsPlugin();
+  //
+  // await flutterLocalNotificationsPlugin
+  //     .resolvePlatformSpecificImplementation<
+  //         AndroidFlutterLocalNotificationsPlugin>()
+  //     ?.createNotificationChannel(channel);
+  //
+  // FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+  //   RemoteNotification? notification = message.notification;
+  //   AndroidNotification? android = message.notification?.android;
+  //
+  //   // If `onMessage` is triggered with a notification, construct our own
+  //   // local notification to show to users using the created channel.
+  //   if (notification != null && android != null) {
+  //     flutterLocalNotificationsPlugin.show(
+  //         notification.hashCode,
+  //         notification.title,
+  //         notification.body,
+  //         NotificationDetails(
+  //           android: AndroidNotificationDetails(
+  //             channel.id,
+  //             channel.name,
+  //             channel.description,
+  //             icon: android.smallIcon,
+  //             // other properties...
+  //           ),
+  //         ));
+  //   }
+  // });
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
   WidgetsFlutterBinding.ensureInitialized();
 
   runApp(MyApp());
